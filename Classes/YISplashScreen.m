76740a1f733db1fcf0e2e462f552c87f23128b6e
Yasuhiro Inami <inamiy@gmail.com>
2013-09-15 15:45:44 +0900

    Don't crop statusBarFrame for iOS7's flat-design.

--- Classes/YISplashScreen.m
+++ Classes/YISplashScreen.m
@@ -8,7 +8,9 @@
 
 #import "YISplashScreen.h"
 
-#define YI_IS_4_INCH    ([UIScreen mainScreen].bounds.size.height == 568.0)
+#define IS_4_INCH               ([UIScreen mainScreen].bounds.size.height == 568.0)
+#define IS_IOS_AT_LEAST(ver)    ([[[UIDevice currentDevice] systemVersion] compare:ver] != NSOrderedAscending)
+#define IS_FLAT_DESIGN          (__IPHONE_OS_VERSION_MAX_ALLOWED >= 70000 && IS_IOS_AT_LEAST(@"7.0"))
 
 static UIViewController* __originalRootViewController = nil;
 static UIWindow* __splashWindow = nil;
@@ -22,7 +24,7 @@ static CALayer* __splashLayer = nil;
     UIWindow* window = [UIApplication sharedApplication].delegate.window;
     
     //
-    // temporally disable rootViewController 
+    // temporally disable rootViewController
     // to avoid calling any CoreData logic while showing splash image
     //
     // (add dummy rootViewController to prevent console warning
@@ -40,7 +42,7 @@ static CALayer* __splashLayer = nil;
     // splash layer (portrait)
     // TODO: show/hide landscape splash image
     CALayer* splashLayer = [CALayer layer];
-    if (YI_IS_4_INCH) {
+    if (IS_4_INCH) {
         splashLayer.contents = (id)[UIImage imageNamed:@"Default-568h.png"].CGImage;
     }
     else {
@@ -53,28 +55,31 @@ static CALayer* __splashLayer = nil;
         
 	    splashLayer.frame = CGRectMake(0, 0, splashLayer.frame.size.width, splashLayer.frame.size.height + statusBarHeight);
         
-	    CAShapeLayer *mask = [[CAShapeLayer alloc] init];
-	    mask.frame = splashLayer.bounds;
-	    mask.fillColor = [[UIColor blackColor] CGColor];
-        
-	    CGFloat x = 0;
-	    CGFloat y = statusBarHeight;
-	    CGFloat width = splashLayer.frame.size.width;
-	    CGFloat height = splashLayer.frame.size.height - statusBarHeight;
-        
-	    CGMutablePathRef path = CGPathCreateMutable();
-        
-	    CGPathMoveToPoint(path, NULL, x, y);
-	    CGPathAddLineToPoint(path, nil, x + width, y);
-	    CGPathAddLineToPoint(path, nil, x + width, y + height);
-	    CGPathAddLineToPoint(path, nil, x, y + height);
-	    CGPathAddLineToPoint(path, nil, x, y);
-	    CGPathCloseSubpath(path);
-        
-	    mask.path = path;
-	    CGPathRelease(path);
-        
-	    splashLayer.mask = mask;
+        if (!IS_FLAT_DESIGN) {
+            CAShapeLayer *mask = [[CAShapeLayer alloc] init];
+            mask.frame = splashLayer.bounds;
+            mask.fillColor = [[UIColor blackColor] CGColor];
+            
+            CGFloat x = 0;
+            CGFloat y = statusBarHeight;
+            CGFloat width = splashLayer.frame.size.width;
+            CGFloat height = splashLayer.frame.size.height - statusBarHeight;
+            
+            CGMutablePathRef path = CGPathCreateMutable();
+            
+            CGPathMoveToPoint(path, NULL, x, y);
+            CGPathAddLineToPoint(path, nil, x + width, y);
+            CGPathAddLineToPoint(path, nil, x + width, y + height);
+            CGPathAddLineToPoint(path, nil, x, y + height);
+            CGPathAddLineToPoint(path, nil, x, y);
+            CGPathCloseSubpath(path);
+            
+            mask.path = path;
+            CGPathRelease(path);
+            
+            splashLayer.mask = mask;
+        }
+	    
 	}
     
     [splashWindow.layer addSublayer:splashLayer];
